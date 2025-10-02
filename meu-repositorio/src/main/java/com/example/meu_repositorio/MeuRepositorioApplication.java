a90f5bf02f1f626a39c58de889998d62f6bd7a19
leoleojogos <leonardo.265021@alunosatc.edu.br>
2025-10-02 20:26:50 -0300

    atualização

---
+++ meu-repositorio/src/main/java/com/example/meu_repositorio/MeuRepositorioApplication.java
@@ -0,0 +1,13 @@
+package com.example.meu_repositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuRepositorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuRepositorioApplication.class, args);
+	}
+
+}
---
+++ meu-repositorio/src/test/java/com/example/meu_repositorio/MeuRepositorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meu_repositorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuRepositorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
